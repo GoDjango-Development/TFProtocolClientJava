99725ba8b2aa42eabb70baf987edc9bb37f98c08
GoDjango LLC <contact@godjango.dev>
2023-11-01 17:23:29 -0400

    feat: Removed src folder from root

--- src/main/java/com/nerox/client/callbacks/IXSRPCProxyCallback.java
+++
@@ -1,9 +0,0 @@
-package com.nerox.client.callbacks;
-
-import com.nerox.client.misc.StatusInfo;
-import com.nerox.client.modules.XSRPCProxy;
-
-public interface IXSRPCProxyCallback extends ISuperCallback<XSRPCProxy>{
-    void startRPCProxyCallback(StatusInfo si);
-    void receivePayload(StatusInfo si);
-}
--- src/main/java/com/nerox/client/misc/StatusServer.java
+++
@@ -1,5 +0,0 @@
-package com.nerox.client.misc;
-
-public enum StatusServer {
-    OK, FAILED, UNKNOWN,CONT,BREAK,DISCONNECTED,PAYLOAD_TOO_BIG
-}
